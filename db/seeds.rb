0d29adb0e701ee80ad72b40fb7b39190858cd2bb
lionel chamouilli <lionel.chamouilli@gmail.com>
2022-03-27 19:06:26 +0200

    New Seed New Background HP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+List.destroy_all
 Bookmark.destroy_all
 Movie.destroy_all
 
